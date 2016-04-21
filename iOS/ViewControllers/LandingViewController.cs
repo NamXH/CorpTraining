b98147c5e2ec30ff42a3eff9a9553aa8bfa0c870
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2016-04-20 22:31:59 -0600

    Fix landing image.

--- iOS/ViewControllers/LandingViewController.cs
+++ iOS/ViewControllers/LandingViewController.cs
@@ -13,32 +13,49 @@ namespace CorpTraining.iOS
         {
             base.ViewDidLoad();
 
-//            View.BackgroundColor = UIColor.FromPatternImage(UIImage.FromBundle("landing.png"));
+            string landingImageName;
+            switch ((int)UIScreen.MainScreen.Bounds.Height)
+            {
+                case (568):
+                    landingImageName = "landing-568h.png";
+                    break;
+                case (667):
+                    landingImageName = "landing-667h.png";
+                    break;
+                case (736):
+                    landingImageName = "landing-736h.png";
+                    break;
+                default:
+                    landingImageName = "landing.png";
+                    break;
+            }
 
-//            var getStartedButton = new UIButton(UIButtonType.System)
-//            {
-//                BackgroundColor = UIColor.FromHSB(UIConstants.MainColorHue, UIConstants.MainColorSaturation, UIConstants.MainColorBrightness),
-//                Font = UIFont.BoldSystemFontOfSize(UIConstants.NormalFontSize),
-//            };
-//            View.AddSubview(getStartedButton);
-//            getStartedButton.SetTitle("GET STARTED", UIControlState.Normal);
-//            getStartedButton.SetTitleColor(UIColor.White, UIControlState.Normal);
-//            getStartedButton.Layer.CornerRadius = 30f;
-//            getStartedButton.TouchUpInside += (sender, e) =>
-//            {
-//                NavigationController.PushViewController(new LoginViewController(), true);
-//            };
+            View.BackgroundColor = UIColor.FromPatternImage(UIImage.FromBundle(landingImageName));
 
-//            #region Layout
-//            View.ConstrainLayout(() =>
-//                getStartedButton.Frame.Top == View.Frame.GetCenterY() + 60f &&
-//                getStartedButton.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
-//                getStartedButton.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
-//                getStartedButton.Frame.GetCenterX() == View.Frame.GetCenterX() &&
-//                getStartedButton.Frame.Height == 70f &&
-//                getStartedButton.Frame.Width == 300f 
-//            );
-//            #endregion
+            var getStartedButton = new UIButton(UIButtonType.System)
+            {
+                BackgroundColor = UIColor.FromHSB(UIConstants.MainColorHue, UIConstants.MainColorSaturation, UIConstants.MainColorBrightness),
+                Font = UIFont.BoldSystemFontOfSize(UIConstants.NormalFontSize),
+            };
+            View.AddSubview(getStartedButton);
+            getStartedButton.SetTitle("GET STARTED", UIControlState.Normal);
+            getStartedButton.SetTitleColor(UIColor.White, UIControlState.Normal);
+            getStartedButton.Layer.CornerRadius = 30f;
+            getStartedButton.TouchUpInside += (sender, e) =>
+            {
+                NavigationController.PushViewController(new LoginViewController(), true);
+            };
+
+            #region Layout
+            View.ConstrainLayout(() =>
+                getStartedButton.Frame.Top == View.Frame.GetCenterY() + 60f &&
+                getStartedButton.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
+                getStartedButton.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
+                getStartedButton.Frame.GetCenterX() == View.Frame.GetCenterX() &&
+                getStartedButton.Frame.Height == 70f &&
+                getStartedButton.Frame.Width == 300f 
+            );
+            #endregion
         }
     }
 }
